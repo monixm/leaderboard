6938336c1db1206a9f0811b5b621da58704aeb7e
monixm <monika.malecka19@gmail.com>
2020-03-02 21:44:56 +0100

    Updates

---
+++ precache-manifest.50a46ebb7ba1b64cfbdf5cf0e8885807.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "accacf0e619969a2b79f18b32da4f665",
+    "url": "/monixm/leaderboard/index.html"
+  },
+  {
+    "revision": "996af3e8b10638f4bac4",
+    "url": "/monixm/leaderboard/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "996af3e8b10638f4bac4",
+    "url": "/monixm/leaderboard/static/js/2.2f0fead2.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/monixm/leaderboard/static/js/2.2f0fead2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f0136bef831f92ddb93",
+    "url": "/monixm/leaderboard/static/js/main.3ed4186c.chunk.js"
+  },
+  {
+    "revision": "a152c25e23471888e4a9",
+    "url": "/monixm/leaderboard/static/js/runtime-main.df97d71d.js"
+  }
+]);
